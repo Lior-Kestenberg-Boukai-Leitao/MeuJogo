8c8047d58c5834cdddc1303fa4d9a00eac491ad8
Lior Kestenberg Boukai Leitão <liorkboukai@gmail.com>
2023-09-29 16:06:25 -0300

    Jogador se Move

--- Joguinho 1/Assets/Movimento.cs
+++ Joguinho 1/Assets/Movimento.cs
@@ -7,12 +7,14 @@ public class Movimento : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+         transform.Translate(xValue,0,zValue);
     }
 }
