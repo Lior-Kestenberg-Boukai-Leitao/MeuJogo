dd2a53c99b75b668aa459c437b8c2268333959d4
Lior Kestenberg Boukai Leitão <liorkboukai@gmail.com>
2023-10-04 08:51:57 -0300

    gravidade

--- Joguinho 1/Assets/Scripts/Gravidade.cs
+++ Joguinho 1/Assets/Scripts/Gravidade.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravidade : MonoBehaviour
-{
+{   
+
+    [SerializeField] float TempoParaCair = 5f;
+    MeshRenderer render;
+    Rigidbody corpo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        render = GetComponent<MeshRenderer>();
+        corpo = GetComponent<Rigidbody>();
+
+        render.enabled = false;
+        corpo.useGravity = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(Time.time )
+        if(Time.time > TempoParaCair)
+        {
+            render.enabled = true;
+            corpo.useGravity = true;
+        }
     }
 }
